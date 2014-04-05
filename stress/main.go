97f06611377494b1610ca4a9eeebb40e05746af4
Damian Gryski <damian@gryski.com>
2014-04-05 13:31:03 +0200

    stress: first draft of rate-based testing

--- stress/main.go
+++ stress/main.go
@@ -35,7 +35,7 @@ func (r Results) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
 func main() {
 
 	host := flag.String("h", "localhost:4444", "shardcache host")
-	clients := flag.Int("c", 10, "number of concurrent clients")
+	clients := flag.Int("clients", 0, "number of concurrent clients")
 	write := flag.Int("w", 50, "percentage of calls which should be writes")
 	keystr := flag.String("k", "stress", "basename key to write to")
 	nkey := flag.Int("nk", 10, "number of keys to set/get")
@@ -43,10 +43,14 @@ func main() {
 	deletekeys := flag.Bool("del", false, "delete keys instead of writing")
 	getIndex := flag.Bool("idx", false, "query shardcache for keys to use instead of generating random keys")
 	timeout := flag.Duration("timeout", 0, "length of time to run")
-	//	rate := flag.Int("rate", 0, "rate (qps)")
+	rate := flag.Int("rate", 0, "rate (qps)")
 	timings := flag.Bool("timings", true, "log response timing metrics")
 
 	flag.Parse()
+
+	if *clients == 0 && *rate == 0 {
+		log.Fatal("must provide one of: -clients / -rate")
+	}
 
 	hosts := strings.Split(*host, ",")
 	nhosts := len(hosts)
@@ -110,100 +114,232 @@ func main() {
 
 	log.Println("starting ...")
 
-	for i := 0; i < *clients; i++ {
+	if *clients > 0 {
 
-		go func(done <-chan struct{}, resultsCh chan<- Results) {
-			var clients []*shardcache.Client
-			for _, h := range hosts {
-				client, err := shardcache.New(h, nil)
+		for i := 0; i < *clients; i++ {
 
-				if err != nil {
-					log.Fatal("unable to create client for host ", h, ": ", err)
+			go func(done <-chan struct{}, resultsCh chan<- Results) {
+				var clients []*shardcache.Client
+				for _, h := range hosts {
+					client, err := shardcache.New(h, nil)
+
+					if err != nil {
+						log.Fatal("unable to create client for host ", h, ": ", err)
+					}
+					clients = append(clients, client)
 				}
-				clients = append(clients, client)
-			}
 
-			results := make(Results, 0, 1000)
+				results := make(Results, 0, 1000)
 
-			rnd := rand.New(rand.NewSource(time.Now().UnixNano() + rand.Int63()))
-			for {
-				key := keys[rnd.Intn(*nkey)]
+				rnd := rand.New(rand.NewSource(time.Now().UnixNano() + rand.Int63()))
+				for {
+					key := keys[rnd.Intn(*nkey)]
 
-				clientNumber := rnd.Intn(nhosts)
-				client := clients[clientNumber]
+					clientNumber := rnd.Intn(nhosts)
+					client := clients[clientNumber]
 
-				var r Result
+					var r Result
 
-				if rnd.Intn(100) < *write {
+					if rnd.Intn(100) < *write {
 
-					r.Write = true
-					if *deletekeys {
+						r.Write = true
+						if *deletekeys {
 
-						if *verbose {
-							log.Println("DEL client=", hosts[clientNumber], "key=", key)
-						}
+							if *verbose {
+								log.Println("DEL client=", hosts[clientNumber], "key=", key)
+							}
 
-						t0 := time.Now()
-						r.Start = t0.UnixNano()
-						r.Err = client.Delete(key)
-						r.Duration = time.Since(t0)
+							t0 := time.Now()
+							r.Start = t0.UnixNano()
+							r.Err = client.Delete(key)
+							r.Duration = time.Since(t0)
 
-						if r.Err != nil {
-							log.Println("error during delete: ", r.Err)
+							if r.Err != nil {
+								log.Println("error during delete: ", r.Err)
+							}
+
+						} else {
+							// set
+
+							if *verbose {
+								log.Println("SET client=", hosts[clientNumber], "key=", key)
+							}
+
+							var v [16]byte
+							vint := atomic.AddUint64(val, 1)
+							binary.BigEndian.PutUint64(v[:], vint)
+
+							t0 := time.Now()
+							r.Start = t0.UnixNano()
+							r.Err = client.Set(key, v[:], 0)
+							r.Duration = time.Since(t0)
+
+							if r.Err != nil {
+								log.Println("error during set: ", r.Err)
+							}
+
 						}
 
 					} else {
-						// set
 
 						if *verbose {
-							log.Println("SET client=", hosts[clientNumber], "key=", key)
+							log.Println("GET client=", hosts[clientNumber], "key=", key)
 						}
-
-						var v [16]byte
-						vint := atomic.AddUint64(val, 1)
-						binary.BigEndian.PutUint64(v[:], vint)
 
 						t0 := time.Now()
 						r.Start = t0.UnixNano()
-						r.Err = client.Set(key, v[:], 0)
+						_, r.Err = client.Get(key)
 						r.Duration = time.Since(t0)
 
 						if r.Err != nil {
-							log.Println("error during set: ", r.Err)
+							log.Println("error during get: ", r.Err)
 						}
-
 					}
 
-				} else {
-
-					if *verbose {
-						log.Println("GET client=", hosts[clientNumber], "key=", key)
+					if *timings {
+						results = append(results, r)
 					}
 
-					t0 := time.Now()
-					r.Start = t0.UnixNano()
-					_, r.Err = client.Get(key)
-					r.Duration = time.Since(t0)
+					select {
+					case <-done:
+						// the channel has been closed -- this is the shutdown signal
+						resultsCh <- results
+						return
+					default:
 
-					if r.Err != nil {
-						log.Println("error during get: ", r.Err)
 					}
 				}
+			}(done, resultsCh)
+		}
 
-				if *timings {
-					results = append(results, r)
-				}
+	} else if *rate > 0 {
+
+		ticks := time.Tick(time.Second / time.Duration(*rate))
+
+		localResults := make(chan Result)
+		rnd := rand.New(rand.NewSource(time.Now().UnixNano() + rand.Int63()))
+		go func() {
+
+			for {
 
 				select {
+				case <-ticks:
+
 				case <-done:
-					// the channel has been closed -- this is the shutdown signal
+					return
+				}
+
+				go func() {
+					key := keys[rnd.Intn(*nkey)]
+
+					host := hosts[rnd.Intn(nhosts)]
+
+					t0 := time.Now()
+					r := Result{
+						Start: t0.UnixNano(),
+					}
+
+					client, err := shardcache.New(host, nil)
+
+					if err != nil {
+						log.Println("error during connect: ", err)
+						r.Err = err
+						r.Duration = time.Since(t0)
+						localResults <- r
+						return
+					}
+
+					if rnd.Intn(100) < *write {
+
+						r.Write = true
+						if *deletekeys {
+
+							if *verbose {
+								log.Println("DEL client=", host, "key=", key)
+							}
+
+							t0 := time.Now()
+							r.Start = t0.UnixNano()
+							r.Err = client.Delete(key)
+							r.Duration = time.Since(t0)
+
+							if r.Err != nil {
+								log.Println("error during delete: ", r.Err)
+							}
+
+						} else {
+							// set
+
+							if *verbose {
+								log.Println("SET client=", host, "key=", key)
+							}
+
+							var v [16]byte
+							vint := atomic.AddUint64(val, 1)
+							binary.BigEndian.PutUint64(v[:], vint)
+
+							t0 := time.Now()
+							r.Start = t0.UnixNano()
+							r.Err = client.Set(key, v[:], 0)
+							r.Duration = time.Since(t0)
+
+							if r.Err != nil {
+								log.Println("error during set: ", r.Err)
+							}
+
+						}
+
+					} else {
+
+						if *verbose {
+							log.Println("GET client=", host, "key=", key)
+						}
+
+						t0 := time.Now()
+						r.Start = t0.UnixNano()
+						_, r.Err = client.Get(key)
+						r.Duration = time.Since(t0)
+
+						if r.Err != nil {
+							log.Println("error during get: ", r.Err)
+						}
+					}
+
+					client.Close()
+
+					localResults <- r
+
+				}()
+			}
+
+		}()
+
+		go func() {
+
+			results := make(Results, 0, 1000)
+			var timeout <-chan time.Time
+			for {
+				// something to collect the results
+				select {
+				case r := <-localResults:
+					if *timings {
+						results = append(results, r)
+					}
+				case <-done:
+					// wait 2 seconds after the done signal for any stragglers
+					done = nil // stop watching
+					timeout = time.After(2 * time.Second)
+
+				case <-timeout:
+					// any outstanding requests when we get the shutdown signal are lost -- we don't wait for them
 					resultsCh <- results
 					return
-				default:
-
 				}
 			}
-		}(done, resultsCh)
+		}()
+
+	} else {
+
 	}
 
 	c := make(chan os.Signal, 1)
@@ -238,11 +374,17 @@ func main() {
 	var results Results
 
 	// and read back the responses so the children all clean up
+
+	if *rate > 0 {
+		*clients = 1
+	}
+
 	for i := 0; i < *clients; i++ {
 		results = append(results, <-resultsCh...)
 	}
 
 	if *timings {
+		log.Println("results from ", len(results), "requests")
 		sort.Sort(results)
 		jenc.Encode(results)
 	}
